705a655f69f34ab1b8bc6079d4c12aca95c1d2c4
Yokesh Jayachandiran <110761485+yokeshwaran01@users.noreply.github.com>
2022-11-13 21:20:17 +0530

    Create schemas.py

---
+++ schemas.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    email: str
+    full_name: str
+    phone: int
+    disabled: bool
+
+    class Config:
+        orm_mode = True
