0a8368e61c53c91c906c6f5eff8ffcf73690f246
Yokesh Jayachandiran <110761485+yokeshwaran01@users.noreply.github.com>
2022-11-13 21:18:40 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,32 @@
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy.ext.asyncio import AsyncSession
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlite3.db"
+
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+Base = declarative_base()
+
+
+async def get_db():
+    session = AsyncSession(engine)
+    try:
+        yield session
+    finally:
+        await session.close()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    full_name = Column(String, index=True)
+    phone = Column(String, index=True)
+    disabled = Column(Boolean, default=False)
+    hashed_password = Column(String)
